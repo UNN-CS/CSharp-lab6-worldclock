f2260ba0ad0051a4c0030eda77f30e1277673133
Stasyan <stanislav.matveew@yandex.ru>
2018-12-20 00:45:10 +0300

    время лечит

---
+++ scr/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace world_clock
+{
+    class Class1
+    {
+        Dictionary<string, TimeSpan> zones = new Dictionary<string, TimeSpan>();
+        public delegate void Update(TextBox tb, DateTime dt);
+        public Update upd;
+        System.Timers.Timer timer;
+        public TextBox tb;
+        string name;
+        public Class1(string name, TextBox tb, Update upd)
+        {
+            zones["World"] = new TimeSpan(-2, 0, 0);
+            zones["Moscow"] = new TimeSpan(0, 0, 0);
+            zones["Vlad"] = new TimeSpan(7, 0, 0);
+            this.upd = upd;
+            this.tb = tb;
+            this.name = name;
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimeEvent;
+
+        }
+
+        public void Start()
+        {
+            timer.Enabled = true;
+        }
+        public void Stop()
+        {
+            timer.Enabled = false;
+        }
+
+        public void OnTimeEvent(Object source, ElapsedEventArgs e)
+        {
+            DateTime dt = DateTime.Now + zones[name];
+            this.upd(tb, dt);
+        }
+    }
+}
---
+++ scr/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace world_clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            clock1 = new Class1("Moscow", textBox1, updateTime);
+            clock2 = new Class1("World", textBox2, updateTime);
+            clock3 = new Class1("Vlad", textBox3, updateTime);
+        }
+
+        private Class1 clock1;
+        private Class1 clock2;
+        private Class1 clock3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void updateTime(TextBox tb, DateTime dt)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tb.Text = dt.ToString();
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clock1.Start();
+            clock2.Start();
+            clock3.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clock1.Stop();
+            clock2.Stop();
+            clock3.Stop();
+        }
+    }
+}
