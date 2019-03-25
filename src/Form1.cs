c52b0db82daad8588e443d292e5628a0287e7980
kvokka123 <and-reed@mail.ru>
2019-03-25 21:37:31 +0300

    6

---
+++ src/Form1.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        private Clock clock1;
+        private Clock clock2;
+        private Clock clock3;
+        public Form1()
+        {
+            InitializeComponent();
+            clock1 = new Clock("World", textBox1, updateTime1);
+            clock2 = new Clock("Moscow", textBox2, updateTime2);
+            clock3 = new Clock("Vlad", textBox3, updateTime3);
+        }
+        public void updateTime1(TextBox tb, DateTime dt)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox1.Text = dt.ToString();
+
+            }));
+        }
+        public void updateTime2(TextBox tb, DateTime dt)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox2.Text = dt.ToString();
+            }));
+        }
+        public void updateTime3(TextBox tb, DateTime dt)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox3.Text = dt.ToString();
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clock1.Start();
+            clock2.Start();
+            clock3.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clock1.Stop();
+            clock2.Stop();
+            clock3.Stop();
+        }
+    }
+    public class Clock
+    {
+        private string zone;
+        public TextBox tb;
+        public delegate void
+        Update(TextBox tb, DateTime dt);
+        public Update upd;
+        System.Timers.Timer timer;
+        Dictionary<string, TimeSpan> zones;
+
+
+        public Clock(string zone, TextBox tb, Update upd)
+        {
+            zones = new Dictionary<string, TimeSpan>();
+            zones["World"] = new TimeSpan(-2, 0, 0);
+            zones["Moscow"] = new TimeSpan(0, 0, 0);
+            zones["Vlad"] = new TimeSpan(7, 0, 0);
+
+            this.zone = zone;
+            this.tb = tb;
+            this.upd = upd;
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimedEvent;
+
+        }
+        public void Start()
+        {
+            timer.Enabled = true;
+        }
+        public void Stop()
+        {
+            timer.Enabled = false;
+        }
+        public void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            dt += zones[zone];
+            this.upd(tb, dt);
+        }
+    }
+}
